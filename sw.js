ffb1c279fdc356a8d86ba3ed4c47ec7ead8f6206
DESKTOP-62E9I02\charg <charger_gt2012@hotmail.com>
2022-06-13 14:29:25 -0500

    Modificamos la ruta del SW

--- sw.js
+++ sw.js
@@ -1,10 +1,10 @@
 importScripts('js/sw-utils.js');
-const STATIC_CACHE = 'static-v1';
+const STATIC_CACHE = 'static-v2';
 const DYNAMIC_CACHE = 'dynamic-v1';
 const INMUTABLE_CACHE = 'inmutable-v1';
 
 const APP_SHELL = [
-    '/',
+    // '/',
     'index.html',
     'css/style.css',
     'img/favicon.ico',
