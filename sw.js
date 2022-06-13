139ae44ce6ae9c677174c9c46f861839472f02b1
DESKTOP-62E9I02\charg <charger_gt2012@hotmail.com>
2022-06-13 16:46:08 -0500

    Modificaciones de iphone x+

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 importScripts('js/sw-utils.js');
-const STATIC_CACHE = 'static-v3';
-const DYNAMIC_CACHE = 'dynamic-v1';
+const STATIC_CACHE = 'static-v4';
+const DYNAMIC_CACHE = 'dynamic-v2';
 const INMUTABLE_CACHE = 'inmutable-v1';
 
 const APP_SHELL = [
@@ -35,6 +35,9 @@ self.addEventListener('activate', e => {
     const response = caches.keys().then(keys => {
         keys.forEach(key => {
             if (key !== STATIC_CACHE && key.includes('static')) {
+                caches.delete(key);
+            }
+            else if (key !== DYNAMIC_CACHE && key.includes('dynamic')) {
                 caches.delete(key);
             }
         });
